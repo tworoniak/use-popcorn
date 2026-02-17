f99c0a703d91476199490bb43de889b637321023
Thomas Woroniak <thomasworoniak@gmail.com>
2026-02-17 15:17:45 -0600

    update

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import StarRating from './components/StarRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* <App /> */}
     <StarRating maxRating={10} />
   </React.StrictMode>,
 );
